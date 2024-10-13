37658fc76463b705cd820f344c486b9ade83b675
Uladzimir <boba70325@gmail.com>
2024-10-13 20:09:25 +0300

    Update "src/s2-homeworks/hw02/affairs/Affairs.tsx"

--- src/s2-homeworks/hw02/affairs/Affairs.tsx
+++ src/s2-homeworks/hw02/affairs/Affairs.tsx
@@ -11,10 +11,10 @@ type AffairsPropsType = {
 }
 
 function Affairs(props: AffairsPropsType) {
-    const setAll = () => props.setFilter('all')
+    const setAll = () => {props.setFilter('all')
         // need to fix
     }
-    const setHigh = () => {
+    const setHigh = () => { props.setFilter('high')
         // need to fix
     }
     const setMiddle = () => {
@@ -23,7 +23,7 @@ function Affairs(props: AffairsPropsType) {
     const setLow = () => {
         // need to fix
     }
-
+}
     const cnAll = s.button + ' ' + s.all + (props.filter === 'all' ? ' ' + s.active : '')
     const cnHigh = s.button + ' ' + s.high + (props.filter === 'high' ? ' ' + s.active : '')
     const cnMiddle = s.button + ' ' + s.middle + (props.filter === 'middle' ? ' ' + s.active : '')
