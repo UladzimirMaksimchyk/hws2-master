78499d4af6e746c2e1fd90a631ff852e10726754
Uladzimir <boba70325@gmail.com>
2024-11-02 06:25:03 +0300

    Update "src/s2-homeworks/hw03/GreetingContainer.tsx"

--- src/s2-homeworks/hw03/GreetingContainer.tsx
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -7,7 +7,7 @@ type GreetingContainerPropsType = {
     addUserCallback:  (name: string) => void // need to fix any
 }
 
-export const pureAddUser = (name:  string, setError:  (error: string) => void, setName: any, addUserCallback:) => {
+export const pureAddUser = (name:  string, setError:  (error: string) => void, setName:   (name: string) => void, addUserCallback:) => {
     // если имя пустое - показать ошибку, иначе - добавить юзера и очистить инпут
 }
 
