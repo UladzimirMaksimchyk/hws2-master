620c36152ef7c4838271bb4fc97d86ceb98bec53
Uladzimir <boba70325@gmail.com>
2024-11-17 15:55:27 +0300

    Update "src/s2-homeworks/hw03/GreetingContainer.tsx"

--- src/s2-homeworks/hw03/GreetingContainer.tsx
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -20,6 +20,7 @@ export const pureAddUser = (name:  string, setError:  (error: string) => void, s
 
 export const pureOnBlur = (name: string, setError:( error: string)) => {
     if (name.trim() === '') {
+        setError('Name is required')
 
     }
     // если имя пустое - показать ошибку
