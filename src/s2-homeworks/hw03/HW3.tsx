f47ec3f9a9299f4fb1d62dfacd4162aefd4a996d
Uladzimir <boba70325@gmail.com>
2024-12-22 14:48:13 +0300

    Update "src/s2-homeworks/hw03/HW3.tsx"

--- src/s2-homeworks/hw03/HW3.tsx
+++ src/s2-homeworks/hw03/HW3.tsx
@@ -23,7 +23,7 @@ export type UserType = {
     name: string // need to fix any
 }
 
-export const pureAddUserCallback = (name: string, setUsers: any, users: any) => { // need to fix any
+export const pureAddUserCallback = (name: string, setUsers: ()=>void, users: any) => { // need to fix any
     const user = { 
         _id: v1(),
         name: name
